9aec922d653ed9f7319f6b032623659e817dbd27
Rafi-99 <rafi.md.2018@gmail.com>
2022-01-19 17:06:32 -0600

    Implemented Reddit fetch

--- src/pages/api/reddit/[subreddit].js
+++ src/pages/api/reddit/[subreddit].js
@@ -1,4 +1,27 @@
 export default async function handler(request, response) {
     const { subreddit } = request.query;
-    response.status(200).json({ subreddit });
+
+    try {
+        const redditFetch = await fetch(`https://www.reddit.com/r/${subreddit}/random.json`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            }
+        });
+
+        const data = await redditFetch.json();
+
+        if (redditFetch.ok && data[0] !== undefined) {
+            const { title, url, ups, downs, num_comments } = data[0].data.children[0].data;
+            response.status(200).json({ title, url, ups, downs, num_comments });
+        }
+        else {
+            const message = redditFetch.status == 403 ? 'Unable to access subreddit. Forbidden response from Reddit.' : 'Something went wrong. Subreddit not found. Please try again with a valid query.';
+            response.status(500).json({ error: `${redditFetch.status}: ${redditFetch.statusText}`, message });
+        }
+    }
+    catch (error) {
+        response.status(500).json({ error: `${error.name}: ${error.message}`, message:'Something went wrong. Please try again with a valid query.' });
+    }
 };
