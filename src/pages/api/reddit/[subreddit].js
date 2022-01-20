704ddb0ea3f1700345288e03672973e21b4d68af
Rafi-99 <rafi.md.2018@gmail.com>
2022-01-20 00:26:22 -0600

    Minor formatting changes and improvements

--- src/pages/api/reddit/[subreddit].js
+++ src/pages/api/reddit/[subreddit].js
@@ -13,12 +13,12 @@ export default async function handler(request, response) {
         const data = await redditFetch.json();
 
         if (redditFetch.ok && data[0] !== undefined) {
-            const { title, url, ups, downs, num_comments } = data[0].data.children[0].data;
-            response.status(200).json({ title, url, ups, downs, num_comments });
+            const { title, permalink, url: image, score, num_comments } = data[0].data.children[0].data;
+            response.status(200).json({ title, url: `https://www.reddit.com${permalink}`, image, score, num_comments });
         }
         else {
-            const message = redditFetch.status == 403 ? 'Unable to access subreddit. Forbidden response from Reddit.' : 'Something went wrong. Subreddit not found. Please try again with a valid query.';
-            response.status(500).json({ error: `${redditFetch.status}: ${redditFetch.statusText}`, message });
+            const message = redditFetch.status === 403 ? 'Unable to access subreddit. Forbidden response from Reddit.' : 'Subreddit not found. Please try again with a valid query.';
+            response.status(500).json({ error: `${redditFetch.status }: ${redditFetch.statusText}`, message });
         }
     }
     catch (error) {
