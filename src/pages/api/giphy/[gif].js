505cf3631f565ce613304788e511f5cfadece077
Rafi-99 <rafi.md.2018@gmail.com>
2022-01-22 12:49:18 -0600

    Implemented better error handling

--- src/pages/api/giphy/[gif].js
+++ src/pages/api/giphy/[gif].js
@@ -6,10 +6,18 @@ const giphyFetch = new GiphyFetch(giphyKey);
 export default async function handler(request, response) {
     const { gif } = request.query;
 
-    await giphyFetch.random({ tag: gif }).then((results) => {
-        const url = results.data.images.original.url;
-        response.status(200).json({ url });
-    }).catch((error) => {
-        response.status(500).json({ error: `${error.name}: ${error.message}`, message:'Something went wrong. Please try again with a valid query.' });
-    });
+    try {
+        const results = await giphyFetch.random({ tag: gif });
+
+        if (results.meta.status === 200  && results.data.id !== 'undefined') {
+            const { url } = results.data;
+            response.status(200).json({ url });
+        }
+        else {
+            response.status(404).json({ error: '404: Not Found', message: 'GIF not found. Please try again with a valid query.' });
+        }
+    }
+    catch (error) {
+        response.status(500).json({ error: `${error.name}: ${error.message}`, message: 'Something went wrong. Please try again with a valid query.' });
+    }
 };
